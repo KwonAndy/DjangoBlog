0efbfbd08a270226359c3e45dc4574b28eff9a5b
Andy <kwona714@hotmail.com>
2018-08-02 18:41:50 -0700

    commit progress for Lesson07

---
+++ mysite/myblog/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from myblog.views import stub_view
+
+urlpatterns = [
+    url(r'^$', stub_view, name="blog_index"),
+    url(r'^posts/(?P<post_id>\d+)/$', stub_view, name="blog_detail"),
+]
